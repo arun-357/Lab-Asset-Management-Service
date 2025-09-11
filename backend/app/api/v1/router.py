4d5c0436f9a48940ab6f605ea647902e4ac94e76
arun-357 <arunachalamnachiappan20@gmail.com>
2025-09-11 08:59:33 +0530

    add:asset

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -3,4 +3,5 @@ from app.api.v1 import assets, reservations, users, auth
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(assets.router, prefix="/assets", tags=["assets"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
